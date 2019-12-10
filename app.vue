a825f4b023ea850efc58241c449e3e8066d722f0
Marcelo Gaspar <drmpgaspar@gmail.com>
2019-12-10 17:52:47 +0100

    Get data of the api with axios

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+ <div>
+    <table>
+        <tbody>
+            <tr v-for="todo in todos" :key="todo.id" >
+               <img v-bind:src="todo.thumbnailUrl"/>
+               <td>{{ todo.title }}</td>  
+            </tr>
+        </tbody>
+    </table>
+ </div> 
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+           todos : null 
+        }
+    },
+    mounted(){
+        this.getTodos();
+    },
+    methods: {
+        getTodos(){
+            axios
+            .get('https://jsonplaceholder.typicode.com/photos')
+               .then( response => {
+                   this.todos = response.data
+               })
+               .catch( e=> console.log(e))
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
